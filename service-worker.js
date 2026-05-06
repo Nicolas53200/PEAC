53a289a20eb31ce8c0ee9d44c0281c4dbcb9e25c
Nicolas53200 <nicolas.morel@sdis53.fr>
2026-05-06 08:39:53 +0200

    service worker v2

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'peac-pwa-v14-candidates';
+const CACHE_NAME = 'peac-pwa-v25-qrfix';
 const ASSETS = [
   './',
   './index.html',
@@ -8,24 +8,41 @@ const ASSETS = [
   './service-worker.js',
   './logo-peac.png',
   './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icons/icon-512.png',
+  './qrcode-gen.js'
+];
+
+const CDN_LIBS = [
+  'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jsQR/1.4.0/jsQR.min.js',
 ];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(async (cache) => {
+      await cache.addAll(ASSETS);
+      await Promise.allSettled(
+        CDN_LIBS.map((url) =>
+          fetch(url).then((r) => { if (r.ok) cache.put(url, r); }).catch(() => {})
+        )
+      );
+    })
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
-
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
       if (cachedResponse) return cachedResponse;
